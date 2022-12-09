5c20baefac043d10cfa6477d692590366e982892
Sagar <93616394+Sagar1329@users.noreply.github.com>
2022-12-09 19:12:59 +0530

    Integration

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_app/constants/routes.dart';
+import 'package:my_app/views/recognition_page.dart';
 import 'package:my_app/views/register_view.dart';
 import 'package:my_app/views/reports_view.dart';
 import 'package:my_app/views/verify_email.dart';
 import 'package:my_app/views/verify_email.dart';
 import 'firebase_options.dart';
 import '../views/login_view.dart';
-import '../views/reports_view.dart' as ReportsView;
+import '../views/reports_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,8 +23,11 @@ void main() {
       routes: {
         loginRoute: (context) => const LoginView(),
         registerRoute: (context) => const RegisterView(),
-        reportsRoute: (context) => const ReportsView.ReportsView(),
-        verifyEmailRoute: (context) => const VerifyEmailView()
+        reportsRoute: (context) => const ReportsView(
+              title: 'Scan Page',
+            ),
+        verifyEmailRoute: (context) => const VerifyEmailView(),
+        recognizePage: (context) => const RecognizePage()
       },
       debugShowCheckedModeBanner: false,
     ),
